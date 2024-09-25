57f2d0281c8f441a29c871d7d876a1afaf126b49
jgalec6 <167015102+jgalec6@users.noreply.github.com>
2024-09-25 11:05:48 -0500

    feat: add main screen function and minor refactoring

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import curses
 import time
 
-def boot_screen(stdscr, boot_time):
-    stdscr.clear()
-
+def init_screen(stdscr, boot_time, message):
     height, width = stdscr.getmaxyx()
-    message = "Booting up..."
-
     y = height // 2
     x = (width - len(message)) // 2
     stdscr.addstr(y, x, message)
@@ -16,24 +12,69 @@ def boot_screen(stdscr, boot_time):
     stdscr.clear()
     stdscr.refresh()
 
+def boot_screen(stdscr):
+    stdscr.clear()
+    init_screen(stdscr, 5, "Booting up...")
+
 def welcome_screen(stdscr):
+    stdscr.clear()
+    stdscr.border()
+    init_screen(stdscr, 2, "Welcome to TermOS!")
+
+def main_screen(stdscr):
+    stdscr.clear()
+
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+    curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_CYAN)
     stdscr.border()
 
-    height, width = stdscr.getmaxyx()
-    message = "Welcome to TermOS!"
+    # Menu items
+    menu_items = ["File", "View", "Tools", "Help"]
+    version = "TermOS v0.2"
 
-    y = height // 2
-    x = (width - len(message)) // 2
-    stdscr.addstr(y, x, message)
+    # https://docs.python.org/3/library/curses.html#curses.window.attron
+    # Add "attr" from "background" applied to the current window
+    stdscr.attron(curses.color_pair(1))
+    x = 2
     
+    # Show menu items
+    for item in menu_items:
+        stdscr.addstr(1, x, item)
+        # Space between items
+        x += len(item) + 2
+
+    # Version tag
+    height, width = stdscr.getmaxyx()
+    stdscr.addstr(1, (width - len(version) - 2), version)
+    # https://docs.python.org/3/library/curses.html#curses.window.attroff
+    # Remove "attr" from "background" applied to the current window
+    stdscr.attroff(curses.color_pair(1))
     stdscr.refresh()
-    stdscr.getch()
 
+    while True:
+        key = stdscr.getch()
+        
+        # Ctrl + Q (ASCII 17) to exit
+        if key == 17:
+            break
+                
 def main(stdscr):
-    boot_time = 5
+    stdscr = curses.initscr()
+    
+    # https://docs.python.org/3/library/curses.html#curses.curs_set
+    # Disable cursor
+    curses.curs_set(0)
 
-    boot_screen(stdscr, boot_time)
+
+    # https://docs.python.org/3/library/curses.html#curses.has_colors
+    # Checks if the terminal can display colors
+    if curses.has_colors():
+        curses.start_color()
+    
+    boot_screen(stdscr)
     welcome_screen(stdscr)
+    main_screen(stdscr)
 
 if __name__ == "__main__":
     curses.wrapper(main)
