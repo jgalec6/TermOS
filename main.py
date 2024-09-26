ec057e858a4143cbbec2e99d6a6dfbd76774128b
jgalec6 <167015102+jgalec6@users.noreply.github.com>
2024-09-26 14:04:31 -0500

    feat(login): validate user credentials from database

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import curses
 import time
+from auth_manager import create_users_table, validate_login
 
-# For testing purposes
-# TODO: Change this to a SQLite file
-VALID_USERNAME = "admin"
-VALID_PASSWORD = "passwd"
 
 def init_screen(stdscr, boot_time, message):
     height, width = stdscr.getmaxyx()
@@ -17,14 +14,17 @@ def init_screen(stdscr, boot_time, message):
     stdscr.clear()
     stdscr.refresh()
 
+
 def boot_screen(stdscr):
     stdscr.clear()
     init_screen(stdscr, 5, "Booting up...")
+
 
 def welcome_screen(stdscr):
     stdscr.clear()
     stdscr.border()
     init_screen(stdscr, 2, "Welcome to TermOS!")
+
 
 def main_screen(stdscr):
     stdscr.clear()
@@ -42,7 +42,7 @@ def main_screen(stdscr):
     # Add "attr" from "background" applied to the current window
     stdscr.attron(curses.color_pair(1))
     x = 2
-    
+
     # Show menu items
     for item in menu_items:
         stdscr.addstr(1, x, item)
@@ -56,21 +56,25 @@ def main_screen(stdscr):
     # Remove "attr" from "background" applied to the current window
     stdscr.attroff(curses.color_pair(1))
     stdscr.refresh()
-    
+
     while True:
         key = stdscr.getch()
         # Ctrl + Q (ASCII 17) to exit
         if key == 17:
             break
 
+
 def login_screen(stdscr):
+    # Make sure the user table exists
+    create_users_table()
+
     attempts = 0
     max_attempts = 3
     height, width = stdscr.getmaxyx()
 
     # Login screen border
     border_height = 10
-    border_width = 40
+    border_width = 50
     login_border_y = (height // 2) - (border_height // 2)
     login_border_x = (width // 2) - (border_width // 2)
 
@@ -79,28 +83,31 @@ def login_screen(stdscr):
         stdscr.border()
 
         # Title
-        stdscr.addstr(login_border_y, login_border_x + 1, "Welcome! Please login :)", curses.A_BOLD)
+        stdscr.addstr(login_border_y, login_border_x + 1,
+                      "Welcome! Please login :)", curses.A_BOLD)
 
         # Draw the frame for input fields
         for y in range(border_height):
             stdscr.addch(login_border_y + y, login_border_x, curses.ACS_VLINE)
-            stdscr.addch(login_border_y + y, login_border_x + border_width - 1, curses.ACS_VLINE)
+            stdscr.addch(login_border_y + y, login_border_x +
+                         border_width - 1, curses.ACS_VLINE)
 
         for x in range(border_width):
             stdscr.addch(login_border_y, login_border_x + x, curses.ACS_HLINE)
-            stdscr.addch(login_border_y + border_height - 1, login_border_x + x, curses.ACS_HLINE)
+            stdscr.addch(login_border_y + border_height - 1,
+                         login_border_x + x, curses.ACS_HLINE)
 
         curses.curs_set(1)
-        
+
         # User input
         user_str = "User:"
         user_input = ""
         # User input position
         user_input_y = login_border_y + 2
         user_input_x = login_border_x + 2 + len(user_str) + 1
-        
+
         stdscr.addstr(user_input_y, login_border_x + 2, user_str)
-        stdscr.move(user_input_y , user_input_x)
+        stdscr.move(user_input_y, user_input_x)
 
         # Gets the input
         while True:
@@ -125,9 +132,9 @@ def login_screen(stdscr):
         pass_str = "Password:"
         pass_input = ""
         # Password input position
-        pass_input_y = login_border_y + 4
+        pass_input_y = login_border_y + 3
         pass_input_x = login_border_x + 2 + len(pass_str) + 1
-        
+
         stdscr.addstr(pass_input_y, login_border_x + 2, pass_str)
         stdscr.move(pass_input_y, pass_input_x)
 
@@ -143,16 +150,19 @@ def login_screen(stdscr):
                 if pass_input:
                     pass_input = pass_input[:-1]
                     stdscr.addstr(pass_input_y, pass_input_x, " " * 20)
-                    stdscr.addstr(pass_input_y, pass_input_x, "*" * len(pass_input))
+                    stdscr.addstr(pass_input_y, pass_input_x,
+                                  "*" * len(pass_input))
                     stdscr.move(pass_input_y, pass_input_x + len(pass_input))
             else:
                 pass_input += chr(key)
-                stdscr.addstr(pass_input_y, pass_input_x, "*" * len(pass_input))
+                stdscr.addstr(pass_input_y, pass_input_x,
+                              "*" * len(pass_input))
                 stdscr.move(pass_input_y, pass_input_x + len(pass_input))
-            
+
         # Auth
-        if user_input == VALID_USERNAME and pass_input == VALID_PASSWORD:
-            stdscr.addstr(login_border_y + 6, login_border_x + 2, "Successful login!", curses.A_BOLD)
+        if validate_login(user_input, pass_input):
+            stdscr.addstr(login_border_y + 5, login_border_x +
+                          2, "Successful login!", curses.A_BOLD)
             curses.curs_set(0)
             stdscr.refresh()
             time.sleep(2)
@@ -160,22 +170,26 @@ def login_screen(stdscr):
             return True
         else:
             # Try again boyo!
+            curses.curs_set(0)
             attempts += 1
-            stdscr.addstr(login_border_y + 6, login_border_x + 2, f"Wrong data!. Remaining attempts: {max_attempts - attempts}", curses.A_BOLD)
+            rem_attempts = max_attempts - attempts
+            stdscr.addstr(login_border_y + 5, login_border_x + 2,
+                          f"Wrong data!. Remaining attempts: {rem_attempts}", curses.A_BOLD)
             stdscr.refresh()
             time.sleep(2)
 
     # If too many attempts, show exit message
-    stdscr.addstr(login_border_y + 8, login_border_x + 2, "Too many failed attempts. Turning off...", curses.A_BOLD)
+    stdscr.addstr(login_border_y + 8, login_border_x + 2,
+                  "Too many failed attempts. Bye!", curses.A_BOLD)
     stdscr.refresh()
     time.sleep(2)
     # Invalid auth, proceed to exit
     return False
-            
+
 
 def main(stdscr):
     stdscr = curses.initscr()
-    
+
     # https://docs.python.org/3/library/curses.html#curses.curs_set
     # Disable cursor
     curses.curs_set(0)
@@ -183,7 +197,7 @@ def main(stdscr):
     # Checks if the terminal can display colors
     if curses.has_colors():
         curses.start_color()
-    
+
     boot_screen(stdscr)
     welcome_screen(stdscr)
 
@@ -191,6 +205,6 @@ def main(stdscr):
         main_screen(stdscr)
     else:
         return
-
+        return
 if __name__ == "__main__":
     curses.wrapper(main)
